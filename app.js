a2a93f02b97ab68897be8ce09ed666295dec5539
Sarah Shuey <sarah.shuey@me.com>
2017-08-15 15:46:10 -0500

    checks

--- app.js
+++ app.js
@@ -2,15 +2,9 @@ const express = require('express');
 const path = require('path');
 const mustacheExpress = require('mustache-express');
 const app = express();
-let list = {
-  name: [ "Learn Node Basics", "Learn Express Basics",
-   "Learn Mustache", "Learn HTML forms with Express",
-   "Learn about authentication",
-   "Learn how to connect to PostgreSQL",
-      "Learn how to create databases",
-    "Learn SQL",
-  "Learn how to connect PostgreSQL from Node",
-    "Learn how to use sequelize" ]}
+let list = [
+  {name: "Learn Node Basics", done: true}, {name :"Learn Express Basics", done: true},
+   {name: "Learn Mustache", done: true}, {name:"Learn HTML forms with Express", done: false}]
 
 
 app.engine('mustache', mustacheExpress());
@@ -20,9 +14,9 @@ app.set('view engine', 'mustache')
 //Listening on root
 app.get('/todo/', function (req, res) {
   // TODO write your code here
-  res.render('todo.mustache',list);
+  res.render('todo.mustache',{todos: list});
 })
 
 app.listen(3000, function () {
-  console.log('Successfully started express application!');
+  console.log('Successfully started express appslication!');
 })
