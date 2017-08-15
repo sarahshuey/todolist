7af40c685b37f9ac4a4053be60b053ed8e1c1566
Sarah Shuey <sarah.shuey@me.com>
2017-08-15 14:17:48 -0500

    list

--- app.js
+++ app.js
@@ -2,18 +2,16 @@ const express = require('express');
 const path = require('path');
 const mustacheExpress = require('mustache-express');
 const app = express();
-let list = [
-    { "todo": "Learn Node Basics" },
-    { "todo": "Learn Express Basics" },
-    { "todo": "Learn Mustache" },
-    { "todo": "Learn HTML forms with Express" },
-    { "todo": "Learn about authentication" },
-    { "todo": "Learn how to connect to PostgreSQL" },
-    { "todo": "Learn how to create databases" },
-    { "todo": "Learn SQL" },
-    { "todo": "Learn how to connect PostgreSQL from Node" },
-    { "todo": "Learn how to use sequelize" }
-  ]
+let list = {
+  name: [ "Learn Node Basics", "Learn Express Basics",
+   "Learn Mustache", "Learn HTML forms with Express",
+   "Learn about authentication",
+   "Learn how to connect to PostgreSQL",
+      "Learn how to create databases",
+    "Learn SQL",
+  "Learn how to connect PostgreSQL from Node",
+    "Learn how to use sequelize" ]}
+
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
@@ -22,7 +20,7 @@ app.set('view engine', 'mustache')
 //Listening on root
 app.get('/todo/', function (req, res) {
   // TODO write your code here
-  res.render('todo.mustache');
+  res.render('todo.mustache',list);
 })
 
 app.listen(3000, function () {
